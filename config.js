239155922cd73900adf5613748d127b867e58d4a
Personal laptop <353753088@qq.com>
2025-03-28 13:20:28 +0800

    pages首页设计

---
+++ config.js
@@ -0,0 +1,34 @@
+const config = {
+  cards: [
+    {
+      title: "比赛日历",
+      description: "即将开始的比赛",
+      link: "https://canjisam.github.io/acmApi/index.html",
+      icon: "calendar",
+      color: "#4CAF50"
+    },
+    {
+      title: "比赛日历",
+      description: "近15天到未来一个月的比赛日历",
+      link: "https://canjisam.github.io/acmApi/calendar.html",
+      icon: "calendar-alt",
+      color: "#2196F3"
+    },
+    {
+      title: "工具",
+      description: "今天吃什么？",
+      link: "https://canjisam.github.io/chooseWhatToEat/",
+      icon: "utensils",
+      color: "#FF9800"
+    },
+    {
+      title: "博客",
+      description: "诒森的博客 - 但愿功不唐捐 May our efforts not be in vain.",
+      link: "https://canjisam.github.io/",
+      icon: "blog",
+      color: "#9C27B0"
+    }
+  ]
+};
+
+export default config;
